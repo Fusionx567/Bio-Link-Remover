e8fcae9049cdd7bfaae7c4648922d9c9523559a7
Fusionx567 <131298526+Fusionx567@users.noreply.github.com>
2024-02-21 01:15:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, MessageHandler, Filters
 import re
 
-# Define your bot token here
+# Replace 'your_bot_token_here' with your actual bot token
 TOKEN = '6873076181:AAEDQa0jwEFLzqE8nJxuLt5tTW73rD4ZFAw'
 
 # Define your link regex pattern
@@ -14,13 +14,19 @@ def delete_messages_with_links(update, context):
         context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
 
 def main():
+    # Create an Updater and pass it your bot's token
     updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
     # Add message handler to filter messages with links in user's bio
     dp.add_handler(MessageHandler(Filters.text & (~Filters.command), delete_messages_with_links))
 
+    # Start the Bot
     updater.start_polling()
+
+    # Run the bot until you send a signal to stop it
     updater.idle()
 
 if __name__ == '__main__':
